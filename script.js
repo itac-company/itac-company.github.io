98305265c370208551675a7931cb9d69b728a493
Богдан Климов фронт <king-bogdan@mail.ru>
2020-04-16 20:57:22 +0300

    Тест образование

--- script.js
+++ script.js
@@ -9,21 +9,15 @@ let clicked = true;
 
 burger.addEventListener('click', () => {
     if (clicked) {
-        headerNav.style.zIndex = '8';
-        navList.classList.add('navListVisible');
         navItem.forEach((item, index) => {
-
-            item.style.animation = `navItemAnim .7s ease-in-out ${index / 30}s forwards`;
+            item.style.animation = `navItemAnim .2s ease-in-out ${index / 30}s forwards`;
             crossIcon.style.transform = "scale(1)";
             burgerIcon.style.transform = "scale(0)";
         })
         clicked = false;
     } else {
-        navList.classList.remove('navListVisible');
-        setTimeout(() => { headerNav.style.zIndex = '-1'; }, 500)
         navItem.forEach((item, index) => {
-
-            item.style.animation = `navItemAnimOut .7s ease-in-out 0.6-${index / 30}s forwards`;
+            item.style.animation = `navItemAnimOut .5s ease-in-out 0.6-${index / 10}s forwards`;
             crossIcon.style.transform = "scale(0)";
             burgerIcon.style.transform = "scale(1)";
         })
@@ -80,15 +74,6 @@ $(document).ready(function () {
 
             //анимируем переход на расстояние - top за 1500 мс
             $('body,html').animate({ scrollTop: top }, 600)
-
-            navList.classList.remove('navListVisible');
-            setTimeout(() => { headerNav.style.zIndex = '-1'; }, 500)
-            navItem.forEach((item, index) => {
-
-                item.style.animation = `navItemAnimOut .7s ease-in-out 0.6-${index / 30}s forwards`;
-                crossIcon.style.transform = "scale(0)";
-                burgerIcon.style.transform = "scale(1)";
-            })
         } else {
 
         }
@@ -152,27 +137,21 @@ const firstReview = document.querySelector('#firstReview');
 const secondReview = document.querySelector('#secondReview');
 const thirdReview = document.querySelector('#thirdReview');
 const checkboxes = document.querySelector('.checkboxes');
-const inputs = document.querySelectorAll('.checkboxes input')
 
 function reviewNextSlide() {
-    reviewSlides[reviewCurrentSlide].style.animation = 'reviewHideAnim .5s forwards';
+    reviewSlides[reviewCurrentSlide].className = 'reviewItem reviewHide';
+    reviewSlides[(reviewCurrentSlide + 2) % reviewSlides.length].className = 'reviewItem';
     reviewCurrentSlide = (reviewCurrentSlide + 1) % reviewSlides.length;
-    reviewSlides[reviewCurrentSlide].style.animation = 'reviewShowAnim .5s forwards';
+    reviewSlides[reviewCurrentSlide].className = 'reviewItem reviewShow';
 }
 
 checkboxes.addEventListener('input', () => {
     clearInterval(reviewSlideInterval)
-    for (let i = 0; i < inputs.length; i++) {
-        console.log((i + 1) % inputs.length)
-        if (inputs[i].checked) {
-            reviewSlides[i].className = 'reviewItem reviewShow';
 
-            reviewSlides[(i + 1) % inputs.length].className = 'reviewItem reviewHide';
-
-        }
-    }
     if (firstReview.checked) {
-
+        reviewSlides[0].className = 'reviewItem reviewShow';
+        reviewSlides[1].className = 'reviewItem reviewHide';
+        reviewSlides[2].className = 'reviewItem reviewHide';
     }
 })
 
@@ -254,6 +233,7 @@ sliderDots.forEach(item => {
 
 const secondThing = document.querySelector('.secondThing');
 const thing = document.querySelector('.thing');
+const firstThing = document.querySelector('.firstThing');
 
 thing.addEventListener('click', () => {
     secondThing.classList.toggle('secondThingUp');
@@ -262,77 +242,35 @@ thing.addEventListener('click', () => {
 const stages = document.querySelector('.stages');
 const things = document.querySelector('.things');
 const thirdThing = document.querySelector('.thirdThing');
-const seventhThing = document.querySelector('.seventhThing');
-const sixthThing = document.querySelector('.sixthThing');
-const fourthThing = document.querySelector('.fourthThing');
-const fifthThing = document.querySelector('.fifthThing');
-const firstThing = document.querySelector('.firstThing');
-const firstThingTwo = document.querySelector('.firstThingTwo');
 const body = document.querySelector('body');
 
 body.addEventListener('wheel', () => {
-
-    if (window.pageYOffset > 3260) {
-        things.style.position = 'fixed'
-        things.style.top = '10vh';
-        things.style.left = '40vw';
-        things.style.marginLeft = '0';
+    console.log(stages.clientHeight);
+    console.log(window.pageYOffset)
+    if (window.pageYOffset > 3600) {
+        things.style.position = 'fixed';
+        things.style.top = '300px';
+        things.style.left = '800px';
     }
-    if (window.pageYOffset > 5500 || window.pageYOffset < 3260) {
+    if (window.pageYOffset > 6500 || window.pageYOffset < 3600) {
         things.style.position = 'static';
-        things.style.marginLeft = '50px';
-    }
-    if (window.pageYOffset > 3400) {
-        secondThing.classList.add('secondThingUp');
-    }
-    if (window.pageYOffset < 3400) {
-        secondThing.classList.remove('secondThingUp');
     }
     if (window.pageYOffset > 3800) {
-        thirdThing.classList.add('thirdThingUp');
+        secondThing.classList.add('secondThingUp');
     }
     if (window.pageYOffset < 3800) {
-        thirdThing.classList.remove('thirdThingUp');
-    }
-    if (window.pageYOffset > 4000) {
-        fourthThing.classList.add('fourthThingUp');
-    }
-    if (window.pageYOffset < 4000) {
-        fourthThing.classList.remove('fourthThingUp');
+        secondThing.classList.remove('secondThingUp');
     }
     if (window.pageYOffset > 4200) {
-        fifthThing.classList.add('fifthThingUp');
+        thirdThing.classList.add('thirdThingUp');
     }
     if (window.pageYOffset < 4200) {
-        fifthThing.classList.remove('fifthThingUp');
+        thirdThing.classList.remove('thirdThingUp');
     }
-    if (window.pageYOffset > 4400) {
-        sixthThing.classList.add('sixthThingUp');
+    if (window.pageYOffset > 4200) {
+        firstThing.style.transform = 'scale(0)';
     }
-    if (window.pageYOffset < 4400) {
-        sixthThing.classList.remove('sixthThingUp');
-    }
-    if (window.pageYOffset > 4600) {
-        seventhThing.classList.add('seventhThingUp');
-    }
-    if (window.pageYOffset < 4600) {
-        seventhThing.classList.remove('seventhThingUp');
-    }
-    if (window.pageYOffset > 4800) {
-        firstThing.classList.add('firstThingOut');
-        firstThingTwo.classList.add('firstThingTwoOut');
-    }
-    if (window.pageYOffset < 4800) {
-        firstThing.classList.remove('firstThingOut');
-        firstThingTwo.classList.remove('firstThingTwoOut');
-    }
+
+
 })
 
-function seven(m) {
-
-    let s = m.split('').slice(-1)
-    console.log(s)
-}
-
-
-seven(1021)
